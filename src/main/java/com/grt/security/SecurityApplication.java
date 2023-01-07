ebef77122efac99e77cf255aaa468085e2fb2617
Govindaraju T <90703039+Govind9617@users.noreply.github.com>
2023-01-07 16:42:04 +0530

    first commit

---
+++ src/main/java/com/grt/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.grt.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
